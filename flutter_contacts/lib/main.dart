4627bc7db4ac06a8343219785ebac062a66284b7
Andre Dang <andredang@air-de-andre.home>
2022-05-19 20:04:47 +0200

    test

--- flutter_contacts/lib/main.dart
+++ flutter_contacts/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'test'),
+      home: const MyHomePage(title: 'test2'),
     );
   }
 }
